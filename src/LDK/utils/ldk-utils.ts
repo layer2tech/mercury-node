7b8a42d7b4cf52a3437bcde5dab139e0a33a15a9
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2023-04-18 11:41:12 +0530

    fix: invoice

--- src/LDK/utils/ldk-utils.ts
+++ src/LDK/utils/ldk-utils.ts
@@ -1,11 +1,51 @@
 import { getLDKClient } from "../init/getLDK.ts";
 import db from "../../db/db.ts";
+import * as lightningPayReq from "bolt11";
+import * as ldk from "lightningdevkit";
 
 export const closeConnections = () => {
   console.log("[ldk-utils.ts]: Closing all the connections");
   let LDK = getLDKClient();
   LDK.netHandler?.stop();
 };
+
+export const createInvoice = (amtInSats: number, invoiceExpirySecs: number, description: string) => {
+  let mSats = ldk.Option_u64Z.constructor_some(BigInt(amtInSats * 1000));
+  let min_final_cltv_expiry_delta = 36;
+
+  let invoice = getLDKClient().channelManager.create_inbound_payment(
+    mSats,
+    invoiceExpirySecs,
+    ldk.Option_u16Z.constructor_some(min_final_cltv_expiry_delta),
+  );
+
+  let payment_hash = invoice.res.get_a();
+  let payment_secret = invoice.res.get_b();
+
+  let encodedInvoice = lightningPayReq.encode({
+    satoshis: amtInSats,
+    timestamp: Date.now(),
+    tags: [
+      {
+        tagName: "payment_hash",
+        data: payment_hash,
+      },
+      {
+        tagName: "payment_secret",
+        data: payment_secret,
+      },
+      {
+        tagName: "description",
+        data: description,
+      },
+    ],
+  });
+
+  let privateKeyHex =
+      "e126f68f7eafcc8b74f54d269fe206be715000f94dac067d1c04a8ca3b2db734";
+  let signedInvoice = lightningPayReq.sign(encodedInvoice, privateKeyHex);
+  return signedInvoice;
+}
 
 // export const createInvoice = (amtInSats: number, invoiceExpirySecs: number, description: string) => {
 //     let mSats = getLDKClient().LDK.Option_u64Z.constructor_some(BigInt(amtInSats * 1000));
--- src/routes/serverRoutes.ts
+++ src/routes/serverRoutes.ts
@@ -1,11 +1,44 @@
 import express from "express";
 const router = express.Router();
-import { closeConnections } from '../LDK/utils/ldk-utils';
+import { closeConnections, createInvoice } from '../LDK/utils/ldk-utils';
 
 router.get("/closeConnections", async function (req, res) {
   // Closing all connections
   closeConnections();
   res.status(200).json({ message: "Connections closed" });
+});
+
+router.post("/generate_invoice", async function (req, res) {
+  try {
+    let invoice_config = {
+      amt_in_sats: req.body.amt_in_sats,
+      invoice_expiry_secs: req.body.invoice_expiry_secs,
+      description: req.body.description,
+    };
+    let invoice = createInvoice(
+      invoice_config.amt_in_sats,
+      invoice_config.invoice_expiry_secs,
+      invoice_config.description
+    );
+    // log("info", `Generating Invoice`);
+    let response = invoice;
+    console.log(response);
+    res.status(200).json(response);
+  } catch (err) {
+    const err_msg = `Bad request: ${err}`;
+    console.log(err_msg);
+    // log("error", err_msg);
+    // log("info", `info - ${err_msg}`);
+    // handle_error(res, err);
+  }
+});
+
+router.post("/sendPayment", async function (req, res) {
+  // send a payment with values posted into this route ->
+});
+
+router.post("/receivePayment", async function (req, res) {
+  // receive a payment
 });
 
 export default router;
