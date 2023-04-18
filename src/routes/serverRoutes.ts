f4eff37a1248eeae7b942f1b391c73f543cad8ef
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2023-04-18 21:45:26 +0530

    remove: generate invoice method

--- src/routes/serverRoutes.ts
+++ src/routes/serverRoutes.ts
@@ -19,31 +19,6 @@ router.get("/closeConnections", async function (req, res) {
   res.status(200).json({ message: "Connections closed" });
 });
 
-router.post("/generate_invoice", async function (req, res) {
-  try {
-    let invoice_config = {
-      amt_in_sats: req.body.amt_in_sats,
-      invoice_expiry_secs: req.body.invoice_expiry_secs,
-      description: req.body.description,
-    };
-    // let invoice = createInvoice(
-    //   invoice_config.amt_in_sats,
-    //   invoice_config.invoice_expiry_secs,
-    //   invoice_config.description
-    // );
-    // log("info", `Generating Invoice`);
-    //let response = invoice;
-    //console.log(response);
-    //res.status(200).json(response);
-  } catch (err) {
-    const err_msg = `Bad request: ${err}`;
-    console.log(err_msg);
-    // log("error", err_msg);
-    // log("info", `info - ${err_msg}`);
-    // handle_error(res, err);
-  }
-});
-
 router.post("/send-payment", async function (req, res) {
   // send a payment with values posted into this route ->
   const invoice_str = req.body.invoice;
