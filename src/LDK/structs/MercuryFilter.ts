902e4c32590f643f0433e0aaa12d2a217a49d59b
RajKadir <r.kadir@se16.qmul.ac.uk>
2023-04-27 19:23:01 +0100

    chore: add logging to filter

--- src/LDK/structs/MercuryFilter.ts
+++ src/LDK/structs/MercuryFilter.ts
@@ -1,14 +1,20 @@
 import { FilterInterface, WatchedOutput } from "lightningdevkit";
 
-class MercuryFilter implements FilterInterface{
-    register_tx(txid: Uint8Array, script_pubkey: Uint8Array): void {
-        // <insert code for you to watch for this transaction on-chain>
-    }
+class MercuryFilter implements FilterInterface {
+  register_tx(txid: Uint8Array, script_pubkey: Uint8Array): void {
+    // <insert code for you to watch for this transaction on-chain>
+    console.log("[MercuryFilter.ts/register_tx]: TXID found:", txid);
+    console.log(
+      "[MercuryFilter.ts/register_tx]: script_pubkey found:",
+      script_pubkey
+    );
+  }
 
-    register_output(output: WatchedOutput): void {
-        // <insert code for you to watch for any transactions that spend this
-        //  output on-chain>
-    }
+  register_output(output: WatchedOutput): void {
+    // <insert code for you to watch for any transactions that spend this
+    //  output on-chain>
+    console.log("[MercuryFilter.ts/register_output]: output found:", output);
+  }
 }
 
 export default MercuryFilter;
