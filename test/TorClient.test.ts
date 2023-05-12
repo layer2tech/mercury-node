120671fd937e5a6e1a785b83011b53bc1d4b45cf
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2023-05-12 22:28:53 +0530

    chore: add test for electrum and tor client

---
+++ test/TorClient.test.ts
@@ -0,0 +1,41 @@
+import TorClient, { TOR_ENDPOINT }  from "../src/LDK/bitcoin_clients/TorClient.mjs";
+
+describe("Tor Client", () => {
+    let torClient: TorClient;
+    let bestBlockHash: any;
+    let blockHeight: any;
+    let latestBlockHeader: any;
+    let txid: any;
+
+    beforeAll(async () => {
+        torClient = new TorClient("");
+    });
+
+    it("getBestBlockHash", async () => {
+        bestBlockHash = await torClient.getBestBlockHash();
+        expect(bestBlockHash).toEqual(expect.any(String));
+    });
+
+    it("getBlockHeight", async () => {
+        blockHeight = await torClient.getBlockHeight();
+        expect(blockHeight).toEqual(expect.any(Number));
+    });
+
+    it("getLatestBlockHeader", async () => {
+        latestBlockHeader = await torClient.getLatestBlockHeader(blockHeight);
+        expect(latestBlockHeader).toEqual(expect.any(String));
+    });
+
+    it("getTxIdData", async () => {
+        txid = (await TorClient.get(`${TOR_ENDPOINT}/electrs/block/${bestBlockHash}/txids`)).data[0];
+        const response = await torClient.getTxIdData(txid);
+        expect(response).toHaveProperty("txid");
+        expect(response).toHaveProperty("vout");
+        expect(response).toHaveProperty("sequence");
+    });
+
+    it("getUtxoSpentData", async () => {
+        const response = await torClient.getUtxoSpentData(txid, 0);
+        expect(response).toHaveProperty("spent");
+    });
+});
