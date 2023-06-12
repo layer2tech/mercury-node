57d472a175a27217c28425dc5c79b26b1d60b81f
RajKadir <r.kadir@se16.qmul.ac.uk>
2023-06-12 16:41:03 +0100

    hotfix: change directory of wasm

--- src/LDK/init/initializeWasm.ts
+++ src/LDK/init/initializeWasm.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 
 export default async function initializeWasm() {
   try {
-    const wasm_file = fs.readFileSync("./liblightningjs.wasm");
+    const wasm_file = await fs.promises.readFile("./liblightningjs.wasm");
     await ldk.initializeWasmFromBinary(wasm_file);
   } catch (e) {
     throw new Error(`[initialiseWasm.ts]: InitialiseWasmError: ${e}`);
