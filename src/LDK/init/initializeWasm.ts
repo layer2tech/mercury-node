7d96014f20d3f7d775ab2c554fe720b4fa1a9a66
RajKadir <r.kadir@se16.qmul.ac.uk>
2023-06-14 17:32:20 +0100

    chore: revert

--- src/LDK/init/initializeWasm.ts
+++ src/LDK/init/initializeWasm.ts
@@ -3,7 +3,9 @@ import fs from "fs";
 
 export default async function initializeWasm() {
   try {
-    const wasm_file = await fs.promises.readFile("./liblightningjs.wasm");
+    const wasm_file = await fs.promises.readFile(
+      "../node_modules/lightningdevkit/liblightningjs.wasm"
+    );
     await ldk.initializeWasmFromBinary(wasm_file);
   } catch (e) {
     throw new Error(`[initialiseWasm.ts]: InitialiseWasmError: ${e}`);
