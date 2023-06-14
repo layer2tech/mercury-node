7d560812e9ac2fc146770189d610c5e48de19363
RajKadir <r.kadir@se16.qmul.ac.uk>
2023-06-14 12:13:41 +0100

    hotfix: include ldk wasm

--- src/LDK/init/initializeWasm.ts
+++ src/LDK/init/initializeWasm.ts
@@ -3,9 +3,7 @@ import fs from "fs";
 
 export default async function initializeWasm() {
   try {
-    const wasm_file = await fs.promises.readFile(
-      "../node_modules/lightningdevkit/liblightningjs.wasm"
-    );
+    const wasm_file = await fs.promises.readFile("./liblightningjs.wasm");
     await ldk.initializeWasmFromBinary(wasm_file);
   } catch (e) {
     throw new Error(`[initialiseWasm.ts]: InitialiseWasmError: ${e}`);
